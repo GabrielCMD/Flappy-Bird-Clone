383e0046b409055430261d88693c73a37cd02fa6
Gabriel CMD <evanogabriel1@gmail.com>
2022-08-30 23:27:26 -0300

    Bird Scripts

---
+++ Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public int Score;
+	public Text scoreText;
+
+	//chamado uma vez para inicializar o jogo
+	void Start(){
+		Time.timeScale = 1;
+	}
+
+	public void RestartGame(){
+		SceneManager.LoadScene (0);
+	}
+}
